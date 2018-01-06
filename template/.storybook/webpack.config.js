46290ee4846871727b86a74bc3ead68808420dc1
Marcello Gesmundo <mgesmundo@yoovant.com>
2018-01-06 15:34:18 +0100

    Add ready to use webpack config with css loaders

---
+++ template/.storybook/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+// load the default config generator.
+const genDefaultConfig = require('@storybook/vue/dist/server/config/defaults/webpack.config.js')
+const globalPath = path.resolve(__dirname, '../')
+
+module.exports = (baseConfig, env) => {
+  const config = genDefaultConfig(baseConfig, env);
+
+  // Extend configuration
+  config.module.rules.push({
+    test: /\.scss$/,
+    loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    include: globalPath
+  })
+  config.module.rules.push({
+    test: /\.sass$/,
+    use: {
+      loader: 'sass-loader',
+      options: {
+        indentedSyntax: true
+      }
+    },
+    include: globalPath
+  })
+  config.module.rules.push({
+    test: /\.less$/,
+    use: {
+      loader: 'less-loader'
+    },
+    include: globalPath
+  })
+  config.module.rules.push({
+    test: /\.styl$/,
+    use: {
+      loader: 'stylus-loader'
+    },
+    include: globalPath
+  })
+  config.module.rules.push({  test: /\.css$/,
+    use: {
+      loader: 'style-loader!css-loader'
+    },
+    include: globalPath
+  })
+  config.module.rules.push({
+    test: /\.(woff|woff2)$/,
+    use: {
+      loader: 'url-loader',
+      options: {
+        name: 'fonts/[hash].[ext]',
+        limit: 5000,
+        mimetype: 'application/font-woff'
+      }
+    },
+    include: globalPath
+  })
+  config.module.rules.push({
+    test: /\.(ttf|eot|svg|png)$/,
+    use: {
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[hash].[ext]'
+        }
+    },
+    include: globalPath
+  })
+
+  return config
+}
